f3a4601f90f8aa02406e35a83210e3d206a01097
wdendugu <wdendugu125@gmail.com>
2023-03-07 18:03:14 -0300

    ready to deploy

--- src/app/store.js
+++ src/app/store.js
@@ -10,7 +10,7 @@ export const store = configureStore ({
     },
     middleware: getDefaultMiddleware => 
         getDefaultMiddleware.apply().concat(apiSlice.middleware),
-    devTools: true
+    devTools: false
 })
 
 setupListeners(store.dispatch)
